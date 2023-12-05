5cd74ae093d4e260081c604dcdd754f8cd8aad59
Maria <marycarol.00@gmail.com>
2023-12-05 12:43:20 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" Component={ Login } />
+        <Route path="/carteira" Component={ Wallet } />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,15 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <form>
+      <label htmlFor="" data-testid="email-input">
+        <input type="email" placeholder="Digite seu email" />
+      </label>
+      <label htmlFor="" data-testid="password-input">
+        <input type="password" placeholder="Digite sua senha" />
+      </label>
+      <button>Entrar</button>
+    </form>
+  );
 }
 
 export default Login;
