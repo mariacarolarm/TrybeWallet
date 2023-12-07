7ce25b6037c0b9a858207910930f10be8e5ec7e2
Maria <marycarol.00@gmail.com>
2023-12-07 12:33:39 -0300

    feat: segundo requisito

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
+import { useSelector } from 'react-redux';
+
 function Header() {
-  return <div>Header</div>;
+  const { email } = useSelector((state: any) => state.user);
+  return (
+    <div>
+      <h2 data-testid="email-field">{ email }</h2>
+      <h2 data-testid="total-field">0</h2>
+      <h2 data-testid="header-currency-field">BRL</h2>
+    </div>
+  );
 }
 
 export default Header;
