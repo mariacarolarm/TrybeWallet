7260926a5fbeef67a7978743dc6904305bb1381a
Maria <marycarol.00@gmail.com>
2023-12-11 23:27:55 -0300

    feat: quinto requisito

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,45 @@
+import { useSelector } from 'react-redux';
+import { ExpensesType, GlobalType } from '../services/types';
+
 function Table() {
-  return <div>Table</div>;
+  const { expenses } = useSelector((state: GlobalType) => state.wallet);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense: ExpensesType) => (
+          <tr key={ expense.id }>
+            <td>{expense.description}</td>
+            <td>{expense.tag}</td>
+            <td>{expense.method}</td>
+            <td>{(Number(expense.value)).toFixed(2)}</td>
+            <td>{expense.exchangeRates[expense.currency].name}</td>
+            <td>{(Number(expense.exchangeRates[expense.currency].ask)).toFixed(2)}</td>
+            <td>
+              {(
+                Number(expense.value)
+      * (expense.exchangeRates[expense.currency]?.ask || 0)
+              ).toFixed(2)}
+            </td>
+            <td>Real</td>
+          </tr>
+        ))}
+
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
